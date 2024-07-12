26467deca8aca6229c3a685c801747abe2f6450a
Queen-Vaibhavi <77841320+Queen-Vaibhavi@users.noreply.github.com>
2024-07-12 15:49:18 +0530

    Add files via upload

--- roll-a-dice.py
+++ roll-a-dice.py
@@ -1,48 +1,49 @@
 import random
-ask = input("do you want to roll a dice? ")
+ask = input("do you want to roll a dice? yes/no ")
 
-no = random.randint(1,6)
-if no == 1:
-    print("[-;-;-;-;-;]")
-    print("[       ]")
-    print("[   0   ]")
-    print("[       ]")
-    print("[-;-;-;-;-;]")
+while(ask == "yes"):
 
-    if no == 2:
-     print("[-;-;-;-;-;]")
-    print("[       ]")
-    print("[   0 0  ]")
-    print("[       ]")
-    print("[-;-;-;-;-;]")
+     no = random.randint(1,6)
+     if no == 1:
+          print("[-;-;-;-;]")
+          print( "[      ]")
+          print( "[   0  ]")
+          print( "[      ]")
+          print("[-;-;-;-;]")
 
-    if no == 3:
-     print("[-;-;-;-;-;]")
-    print("[       ]")
-    print("[  0  0  ]")
-    print("[    0    ]")
-    print("[-;-;-;-;-;]")
+     if no == 2:
+          print("[-;-;-;-;]")
+          print( "[      ]")
+          print( "[  0 0 ]")
+          print( "[      ]")
+          print("[-;-;-;-;]")
+     if no == 3:
+          print("[-;-;-;-;]")
+          print( "[      ]")
+          print( "[  0 0 ]")
+          print( "[   0  ]")
+          print("[-;-;-;-;]")
 
-    if no == 4:
-     print("[-;-;-;-;-;]")
-    print("[  0  0   ]")
-    print("[       ]")
-    print("[  0  0   ]")
-    print("[-;-;-;-;-;]")
+     if no == 4:
+          print("[-;-;-;-;]")
+          print("[  0  0  ]")
+          print("[        ]")
+          print("[  0  0  ]")
+          print("[-;-;-;-;]")
 
-    if no == 5:
-     print("[-;-;-;-;-;]")
-    print("[ 0    0  ]")
-    print("[   0   ]")
-    print("[ 0    0 ]")
-    print("[-;-;-;-;-;]")
+     if no == 5:
+          print("[-;-;-;-;]")
+          print("[  0  0  ]")
+          print("[    0   ]")
+          print("[  0  0  ]")
+          print("[-;-;-;-;]")
+     
+     if no == 6:
+          print("[-;-;-;-;]")
+          print("[  0  0  ]")
+          print("[  0  0  ]")
+          print("[  0  0  ]")
+          print("[-;-;-;-;]")
 
-    if no == 6:
-     print("[-;-;-;-;-;]")
-    print("[  0  0   ]")
-    print("[  0  0 ]")
-    print("[  0  0 ]")
-    print("[-;-;-;-;-;]")
-
-
-    
+     response = input("do you want to roll it again?")
+     ask = response    
